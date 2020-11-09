4f4b113e1caf83e2f9efb4a99d8419e58c299588
Max Romanyuta <xom9ik.code@gmail.com>
2020-11-09 11:25:45 +0200

    feat(card): animate gallery height

--- src/components/MiniGallery/index.tsx
+++ src/components/MiniGallery/index.tsx
@@ -1,16 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 
-import SwiperCore, { Navigation } from 'swiper';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import { useCollapse } from '@/use/animationHeight';
 
 interface IMiniGallery {
   id?: number,
   isCollapse: boolean,
 }
-SwiperCore.use([Navigation]);
+SwiperCore.use([Navigation, Pagination]);
 
 const attachments = [{
   extension: 'png',
@@ -31,40 +33,53 @@ const attachments = [{
 }, {
   extension: 'png',
   name: 'a.png',
-  path: 'https://images.unsplash.com/photo-1604542246839-5c3c6fb837fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=280&h=800',
+  path: 'https://images.unsplash.com/photo-1604542246839-5c3c6fb837fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=280&h=2800',
 }];
 
 export const MiniGallery: FC<IMiniGallery> = ({
   isCollapse,
-}) => (
-  <div
-    className={`mini-gallery 
-  ${isCollapse ? 'mini-gallery--collapse' : ''}`}
-    onClick={(e) => e.stopPropagation()}
-    onDoubleClick={(e) => e.stopPropagation()}
-  >
-    <div className="mini-gallery__wrapper">
-      <Swiper
-        slidesPerView={1}
-        height={100}
-        spaceBetween={4}
-        loop
-        navigation
-        setWrapperSize
-        autoHeight
-        onSlideChange={(s) => console.log('swiper slide change', s.activeIndex)}
-        onSwiper={(swiper) => {
-          console.log('swiper onSwiper', swiper.width);
-        }}
-      >
-        {
-          attachments.map((attachment) => (
-            <SwiperSlide key={attachment.path}>
-              <img src={attachment.path} alt="" />
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
+}) => {
+  const { collapse, expand } = useCollapse();
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    if (isCollapse) {
+      collapse(ref.current, 300);
+    } else {
+      expand(ref.current, 300);
+    }
+  }, [isCollapse]);
+
+  return (
+    <div
+      ref={ref}
+      className="mini-gallery"
+      // className={`mini-gallery
+  // ${isCollapse ? 'mini-gallery--collapse' : ''}`}
+      onClick={(e) => e.stopPropagation()}
+      onDoubleClick={(e) => e.stopPropagation()}
+    >
+      <div className="mini-gallery__wrapper">
+        <Swiper
+          slidesPerView={1}
+          height={100}
+          spaceBetween={4}
+          loop
+          setWrapperSize
+          autoHeight
+          navigation
+          effect="flip"
+          pagination={{ type: 'fraction' }}
+        >
+          {
+              attachments.map((attachment) => (
+                <SwiperSlide key={attachment.path}>
+                  <img src={attachment.path} alt="" />
+                </SwiperSlide>
+              ))
+            }
+        </Swiper>
+      </div>
     </div>
-  </div>
-);
+  );
+};
