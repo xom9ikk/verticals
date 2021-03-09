52ae651a7a00615ee5d4362a0b13b78934bbb9e2
Max Romanyuta <xom9ik.code@gmail.com>
2021-03-09 17:12:21 +0200

    test: setup stryker

--- jest.config.js
+++ jest.config.js
@@ -5,17 +5,10 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
-  // setupFilesAfterEnv: ["<rootDir>/internals/jestSettings.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   moduleNameMapper,
-  // moduleNameMapper: {
-  // // https://jestjs.io/docs/en/webpack#handling-static-assets
-  // //   "\\.(css|less)$": "<rootDir>/internals/__mocks__/styleMock.js",
-  // //   "^@/(.*)$": "<rootDir>/src/$1",
-  // },
-  // testPathIgnorePatterns: ["e2e"],
   moduleDirectories: ["node_modules", "src"],
   coverageThreshold: {
     global: {
