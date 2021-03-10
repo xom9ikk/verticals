bf5fab5bc69d8df1000f18aa0f31133c72cd1015
Max Romanyuta <xom9ik.code@gmail.com>
2021-03-10 22:40:56 +0200

    test(auth): add cases

--- src/store/sagas/auth.spec.ts
+++ src/store/sagas/auth.spec.ts
@@ -6,6 +6,7 @@ import { AuthActions } from '@store/actions';
 import { useAlert } from '@use/alert';
 import { AuthService } from '@services/auth';
 import { redirectTo } from '@router/history';
+import { storage } from '@plugins/storage';
 
 // @ts-ignore
 const authService = new AuthService();
@@ -15,6 +16,33 @@ jest.mock('@/i18n', () => ({
 }));
 
 describe('Auth saga', () => {
+  it('sign up', () => {
+    const actionPayload = {
+      name: 'John',
+      surname: 'Doe',
+      username: 'john.doe',
+      email: 'email@test.com',
+      password: 'password123',
+    };
+
+    const mockData = {
+      token: 'token',
+      refreshToken: 'refresh-token',
+    };
+
+    return expectSaga(watchAuth, authService)
+      .provide([
+        [matchers.apply.fn(authService.signUp), {
+          data: mockData,
+        }],
+      ])
+      .dispatch(AuthActions.signUp(actionPayload))
+      .apply(authService, authService.signUp, [actionPayload])
+      .put(AuthActions.setAuthInfo(mockData))
+      .call(show, 'Success', 'Registration completed successfully', ALERT_TYPES.SUCCESS)
+      .call(redirectTo, '/')
+      .silentRun();
+  });
   it('sign in', () => {
     const actionPayload = {
       email: 'email@test.com',
@@ -22,8 +50,8 @@ describe('Auth saga', () => {
     };
 
     const mockData = {
-      token: 'Token',
-      refreshToken: 'refresh',
+      token: 'token',
+      refreshToken: 'refresh-token',
     };
 
     return expectSaga(watchAuth, authService)
@@ -37,6 +65,62 @@ describe('Auth saga', () => {
       .put(AuthActions.setAuthInfo(mockData))
       .call(show, 'Success', 'Successful login', ALERT_TYPES.SUCCESS)
       .call(redirectTo, '/')
+      .silentRun();
+  });
+  it('set auth info', () => {
+    const actionPayload = {
+      token: 'token',
+      refreshToken: 'refresh-token',
+    };
+
+    return expectSaga(watchAuth, authService)
+      .dispatch(AuthActions.setAuthInfo(actionPayload))
+      .call(storage.setToken, actionPayload.token)
+      .call(storage.setRefreshToken, actionPayload.refreshToken)
+      .silentRun();
+  });
+  it('logout', () => expectSaga(watchAuth, authService)
+    .provide([
+      [matchers.apply.fn(authService.logout), undefined],
+    ])
+    .dispatch(AuthActions.logout())
+    .apply(authService, authService.logout, [])
+    .call(show, 'Success', 'Successful logout', ALERT_TYPES.SUCCESS)
+    .put(AuthActions.setAuthInfo({
+      token: '',
+      refreshToken: '',
+    }))
+    .call(redirectTo, '/')
+    .silentRun());
+  it('reset password', () => {
+    const actionPayload = {
+      email: 'email@test.com',
+      password: 'password123',
+    };
+
+    return expectSaga(watchAuth, authService)
+      .provide([
+        [matchers.apply.fn(authService.reset), undefined],
+      ])
+      .dispatch(AuthActions.reset(actionPayload))
+      .apply(authService, authService.reset, [actionPayload])
+      .call(show, 'Success', 'Successful reset password', ALERT_TYPES.SUCCESS)
+      .call(redirectTo, '/auth/login')
+      .silentRun();
+  });
+  it('change password', () => {
+    const actionPayload = {
+      oldPassword: 'password123',
+      newPassword: 'new-password123',
+    };
+
+    return expectSaga(watchAuth, authService)
+      .provide([
+        [matchers.apply.fn(authService.change), undefined],
+      ])
+      .dispatch(AuthActions.changePassword(actionPayload))
+      .apply(authService, authService.change, [actionPayload])
+      .call(show, 'Success', 'Successful change password', ALERT_TYPES.SUCCESS)
       .silentRun();
   });
 });
